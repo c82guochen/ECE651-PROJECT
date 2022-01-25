51a30c582bb043c7a065578af6c20c595f0fcfc1
Eugene Wang <yujing.wang@ontariohealth.ca>
2022-01-25 11:49:48 -0500

    deploy

--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -1,6 +1,6 @@
 import { environment } from 'src/environments/environment'
 
-export const baseUrl = 'http://localhost:3000'
+export const baseUrl = environment.production? 'https://my-json-server.typicode.com/eugland/lehfront' : 'http://localhost:3000'
 export const productsUrl = baseUrl + '/products'
 export const cartUrl = baseUrl + '/cart'
 export const favoriteUrl = baseUrl + '/wishlist'
