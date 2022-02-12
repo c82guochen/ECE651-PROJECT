088fffd4072a3005b06653bbaab81d2179b00ece
Eugene Wang <yujing.wang@ontariohealth.ca>
2022-02-12 17:25:41 -0500

    updated

--- src/app/model/order.ts
+++ src/app/model/order.ts
@@ -5,15 +5,11 @@ export class OrderItem {
   productId: number;
   qty: number;
   product: Product;
-  orderTime: string;
-  deliverTime: string;
 
   constructor(id: number, productId: number, qty: number) {
     this.id = id;
     this.productId = productId;
     this.qty = qty;
     this.product = new Product(0, 'placeholder');
-    this.orderTime = orderTime;
-    this.deliverTime = deliverTime;
   }
 }
