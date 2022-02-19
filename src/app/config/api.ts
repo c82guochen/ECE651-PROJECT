67b63970704cb1206bb0f33a310167f3198bcfbd
Eugene Wang <yujing.wang@ontariohealth.ca>
2022-02-18 19:36:59 -0500

    swapped url

--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -1,8 +1,9 @@
 import { environment } from 'src/environments/environment';
 
-export const baseUrl = environment.production
-  ? 'https://my-json-server.typicode.com/eugland/lehfront'
-  : 'http://localhost:3000';
+export const baseUrl = 'http://34.130.126.193:8000/api'
+// environment.production
+//   ? 'https://my-json-server.typicode.com/eugland/lehfront'
+//   : 'http://localhost:3000';
 export const productsUrl = baseUrl + '/products';
 export const recipesUrl = baseUrl + '/recipes';
 export const cartUrl = baseUrl + '/cart';
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,24 +1,33 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map, Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, Observable, throwError } from 'rxjs';
 import { Product } from '../model/product';
 
 import { productsUrl } from 'src/app/config/api';
+
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+
   local_products = new Map<number, Product>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   getProduct(id: number): Product {
     return this.local_products.get(id) as Product;
   }
 
+  httpOptions = {
+    headers: new HttpHeaders()
+  };
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(productsUrl).pipe(
+    return this.http.get<Product[]>(productsUrl, this.httpOptions).pipe(
       map((res: Product[]) => {
         console.log('get product invoke ');
         for (let item of res) {
@@ -26,6 +35,10 @@ export class ProductService {
         }
         console.log(this.local_products);
         return res;
+      }),
+      catchError(err => {
+        console.log(err);
+        return throwError(err);
       })
     );
   }
